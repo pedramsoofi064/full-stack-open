ec1842c6642c9c701368e2038009a2d0ad3f3a19
Pedram <pedramsoofi064@gmail.com>
2025-12-26 18:09:32 +0200

    feat: Task 2.11

--- part2/phone-book/src/App.jsx
+++ part2/phone-book/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
+import axios from "axios";
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [filteredPersons, setFilteredPersons] = useState(persons);
 
   const handleSetPersons = ({ newName, newNumber }) => {
@@ -22,6 +18,17 @@ const App = () => {
     );
     setFilteredPersons(filteredPersons);
   };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        setPersons(response.data);
+        setFilteredPersons(response.data);
+      })
+      .catch(() => setPersons([]));
+  }, []);
+
   return (
     <div>
       <h2>Phonebook</h2>
