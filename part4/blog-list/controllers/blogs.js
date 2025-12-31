c5775b3abc0a4b0f8f46aafcacb13c8c7d9e30de
Pedram <pedramsoofi064@gmail.com>
2025-12-31 21:30:43 +0200

    Exercises 4.1.-4.2.

---
+++ part4/blog-list/controllers/blogs.js
@@ -0,0 +1,68 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blogs.js')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+})
+
+blogsRouter.get('/:id', (request, response, next) => {
+  const { id } = request.params
+
+  Blog.findById(id)
+    .then(blog => {
+      if (blog) {
+        response.json(blog)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.post('/', (request, response, next) => {
+  const { title, author, url, likes } = request.body
+  const blog = new Blog({
+    title, author, url, likes
+  })
+
+  blog.save()
+    .then(savedBlog => {
+      response.json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.delete('/:id', (request, response, next) => {
+  const { id } = request.params
+  Blog.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.put('/:id', (request, response, next) => {
+  const { id } = request.params
+  const { title, author, url, likes } = request.body
+
+  Blog.findById(id)
+    .then(blog => {
+      if (!blog) {
+        return response.status(404).end()
+      }
+
+      blog.author = author
+      blog.title = title
+      blog.url = url
+      blog.likes = likes
+
+      return blog.save().then((updatedBlog) => {
+        response.json(updatedBlog)
+      })
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
